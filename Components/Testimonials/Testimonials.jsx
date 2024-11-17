7ee509d8c6c3210200e0c9c69e76d50813fe74ca
Karishmita05 <144591004+Karishmita05@users.noreply.github.com>
2024-11-17 21:38:30 +0530

    Add files via upload

---
+++ Components/Testimonials/Testimonials.jsx
@@ -0,0 +1,93 @@
+import React, { useRef } from 'react'
+import './Testimonials.css'
+import next_icon from '../../assets/next-icon.png'
+import back_icon from '../../assets/back-icon.png'
+import user_1 from '../../assets/user-1.png'
+import user_2 from '../../assets/user-2.png'
+import user_3 from '../../assets/user-3.png'
+import user_4 from '../../assets/user-4.png'
+const Testimonials = () => {
+
+    const slider= useRef();
+    let tx=0;
+
+    const slideForward=()=>{
+        if(tx > -50){
+            tx-=25;
+        }
+        slider.current.style.transform = `translateX(${tx}%)`;
+    }
+    const slideBackward=()=>{
+        if(tx < 0){
+            tx+=25;
+        }
+        slider.current.style.transform = `translateX(${tx}%)`;
+    }
+
+
+  return (
+    <div className='testimonials'>
+        <img src={next_icon} alt=" " className='next-btn' onClick={slideForward}/>
+        <img src={back_icon} alt=" " className='back-btn' onClick={slideBackward}/>
+      <div className='slider'>
+        <ul ref={slider}>
+            <li>
+                <div className="slide">
+                    <div className="user-info">
+                        <img src={user_1} alt=" "/>
+                        <div>
+                            <h3>Name1</h3>
+                            <span>loation1</span>
+
+                        </div>
+                    </div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi fugiat quasi ex magnam. Dignissimos aliquid vero beatae veritatis doloribus sunt nihil voluptatem nulla, optio minus temporibus dolor doloremque dolore rerum.</p>
+                </div>
+            </li>
+            <li>
+                <div className="slide">
+                    <div className="user-info">
+                        <img src={user_2} alt=" "/>
+                        <div>
+                            <h3>Name2</h3>
+                            <span>location2</span>
+
+                        </div>
+                    </div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi fugiat quasi ex magnam. Dignissimos aliquid vero beatae veritatis doloribus sunt nihil voluptatem nulla, optio minus temporibus dolor doloremque dolore rerum.</p>
+                </div>
+            </li>
+            <li>
+                <div className="slide">
+                    <div className="user-info">
+                        <img src={user_3} alt=" "/>
+                        <div>
+                            <h3>Name3</h3>
+                            <span>location3</span>
+
+                        </div>
+                    </div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi fugiat quasi ex magnam. Dignissimos aliquid vero beatae veritatis doloribus sunt nihil voluptatem nulla, optio minus temporibus dolor doloremque dolore rerum.</p>
+                </div>
+            </li>
+            <li>
+                <div className="slide">
+                    <div className="user-info">
+                        <img src={user_4} alt=" "/>
+                        <div>
+                            <h3>Name4</h3>
+                            <span>location4</span>
+
+                        </div>
+                    </div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi fugiat quasi ex magnam. Dignissimos aliquid vero beatae veritatis doloribus sunt nihil voluptatem nulla, optio minus temporibus dolor doloremque dolore rerum.</p>
+                </div>
+            </li>
+            
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Testimonials
